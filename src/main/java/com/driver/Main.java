b9d4afb23dc63262a46553e904d6deed6408fcfb
Hitesh <gaikwadhitesh03@gamil.com>
2022-12-11 13:22:43 +0530

    new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly r=new RWOnly();
+//        r.name="hello";
+//        System.out.println(r.name);
+        //name has private access in com.driver.RWOnly
+        r.setName("hello");
+        System.out.println(r.getName());
+    }
 }
